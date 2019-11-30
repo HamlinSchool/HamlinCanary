596b92b70aee16fd9a019245c9df8a91a39d2a8a
Arthur Cinader <700572+acinader@users.noreply.github.com>
2019-11-30 14:51:19 -0800

    pull out is dupe logic and comment

--- index.js
+++ index.js
@@ -1,30 +1,4 @@
-const AWS = require('aws-sdk');
-
-const addDays = require('date-fns/addDays');
-AWS.config.update({region: 'us-west-1' });
-const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
-
-const eventIsDupe = function eventIsDupe(event) {
-  const params = {
-    TableName: 'CanaryLog',
-    Item: {
-      'id' : {S: event.id},
-      'eventTime': { S: event.time },
-      'expires': {S: addDays(new Date(event.time), 1).toISOString() }
-    },
-    ConditionExpression: 'attribute_not_exists(id)',
-  };
-
-  // Call DynamoDB to add the item to the table
-  return dynamodb.putItem(params).promise()
-    .then(() => true)
-    .catch((e) => {
-      if (e.code === 'ConditionalCheckFailedException') {
-          return false;
-      }
-      return Promise.reject(e);
-    });
-};
+const { eventIsDupe } = require('./src/event-is-dupe');
 
 exports.handler = async (event) => {
   if (!process.env.TEST && await eventIsDupe(event)) {
---
+++ src/event-is-dupe.js
@@ -0,0 +1,48 @@
+const addDays = require('date-fns/addDays');
+const AWS = require('aws-sdk');
+
+AWS.config.update({region: 'us-west-1' });
+const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+/**
+ * Determine if this event has already been processed
+ *
+ * AWS CloudWatch delivers events AT LEAST ONCE, so sometimes we'll get dupes.
+ * We don't want to send duplicate notifications, so we de dupe events here
+ *
+ * We use AWS DynamoDB to store a record with the id of the event.
+ * When we go to put a new record in, if its is a duplicate, we know
+ * we've already handled this event.
+ *
+ * The table `CanaryLog`is configured to expire records after a day.  Realistically,
+ * we'd only get  dupe with in a few hundred milliseconds. If it turns out that
+ * we need to keep data for graphing, etc. this would be the place to store it
+ * and then you'd want to turn off the expiration.  The data is very small,
+ * so storing hundreds of years of observations would trivial.
+ *
+ * @param {*} event
+ * @returns promise<boolean> true if this event has already been processed
+ */
+const eventIsDupe = function eventIsDupe(event) {
+
+  const params = {
+    TableName: 'CanaryLog',
+    Item: {
+      'id' : {S: event.id},
+      'eventTime': { S: event.time },
+      'expires': {S: addDays(new Date(event.time), 1).toISOString() }
+    },
+    ConditionExpression: 'attribute_not_exists(id)',
+  };
+
+  return dynamodb.putItem(params).promise()
+    .then(() => true)
+    .catch((e) => {
+      if (e.code === 'ConditionalCheckFailedException') {
+          return false;
+      }
+      return Promise.reject(e);
+    });
+};
+
+module.exports = { eventIsDupe };
